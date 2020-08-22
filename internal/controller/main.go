438014036cead9c1beb1fa8344a3ea0fdead769a
bst27 <git@roesmann.net>
2020-08-22 12:26:55 +0200

    add route to create new comment

--- internal/controller/main.go
+++ internal/controller/main.go
@@ -1,6 +1,8 @@
 package controller
 
 import (
+	"fmt"
+	"github.com/bst27/plaudern/internal/model/api/comment"
 	"github.com/gin-gonic/gin"
 	"net/http"
 	"time"
@@ -11,5 +13,20 @@ func RegisterRoutes(r *gin.Engine) {
 		c.JSON(http.StatusOK, gin.H{
 			"Time": time.Now().Unix(),
 		})
+	})
+
+	r.POST("/comment", func(c *gin.Context) {
+		newComment, err := comment.FromRequest(c)
+
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{
+				"Error": err.Error(),
+			})
+			return
+		}
+
+		fmt.Println(newComment) // TODO: Persist comment
+
+		c.JSON(http.StatusOK, gin.H{})
 	})
 }
