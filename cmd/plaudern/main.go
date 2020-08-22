2579e7e0fdb8da8e95392186dbb1cc3c933a5f29
bst27 <git@roesmann.net>
2020-08-22 11:56:17 +0200

    add http server with ping route

--- cmd/plaudern/main.go
+++ cmd/plaudern/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"time"
+)
 
+func main() {
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"Time": time.Now().Unix(),
+		})
+	})
+
+	log.Println(r.Run(":8080"))
 }
