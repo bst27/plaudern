6e0488e1d014be07a239fb08cc64bd19dcac7445
bst27 <git@roesmann.net>
2020-08-23 16:54:27 +0200

    add support to load saved comments via repository

--- internal/model/comment/comment.go
+++ internal/model/comment/comment.go
@@ -24,6 +24,14 @@ func New(msg string) (*Comment, error) {
 	}, nil
 }
 
+func load(id string, created time.Time, message string) *Comment {
+	return &Comment{
+		id:      id,
+		created: created,
+		message: message,
+	}
+}
+
 func IsValidMessage(message string) bool {
 	return message != ""
 }
--- internal/model/comment/repository.go
+++ internal/model/comment/repository.go
@@ -15,3 +15,34 @@ func Save(comment *Comment, db *sql.DB) error {
 
 	return err
 }
+
+func GetAll(db *sql.DB) ([]*Comment, error) {
+	rows, err := db.Query("SELECT id, created, message FROM comments ORDER BY created DESC")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	var comments []*Comment
+	for rows.Next() {
+		var id, created, message string
+		if err = rows.Scan(&id, &created, &message); err != nil {
+			return nil, err
+		}
+
+		t, err := time.Parse(time.RFC3339, created)
+		if err != nil {
+			return nil, err
+		}
+
+		comment := load(id, t, message)
+
+		comments = append(comments, comment)
+	}
+
+	if err = rows.Err(); err != nil {
+		return nil, err
+	}
+
+	return comments, nil
+}
