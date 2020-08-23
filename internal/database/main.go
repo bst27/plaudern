7fef724d8a6d4b911d5c87e906089a712fd4725e
bst27 <git@roesmann.net>
2020-08-23 16:37:07 +0200

    add in-memory db

---
+++ internal/database/main.go
@@ -0,0 +1,38 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	dbInstance *sql.DB
+)
+
+func Open() (*sql.DB, error) {
+	if dbInstance != nil {
+		return dbInstance, nil
+	}
+
+	db, err := sql.Open("sqlite3", ":memory:") // TODO: Use persistent database
+	if err != nil {
+		return nil, err
+	}
+
+	setup(db)
+
+	dbInstance = db
+	return Open()
+}
+
+func setup(db *sql.DB) error {
+	sqlStmt := `
+		CREATE TABLE comments (
+			id TEXT PRIMARY KEY NOT NULL,
+			created TEXT NOT NULL,
+			message TEXT NOT NULL
+		);
+	`
+	_, err := db.Exec(sqlStmt)
+	return err
+}
