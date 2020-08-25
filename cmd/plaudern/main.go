73740bd3c72a31457d63cf6a429f2c2a620a0da3
bst27 <git@roesmann.net>
2020-08-25 10:09:33 +0200

    use config

--- cmd/plaudern/main.go
+++ cmd/plaudern/main.go
@@ -1,17 +1,20 @@
 package main
 
 import (
+	"github.com/bst27/plaudern/internal/configuration"
 	"github.com/bst27/plaudern/internal/controller"
 	"github.com/bst27/plaudern/internal/middleware"
 	"github.com/gin-gonic/gin"
 	"log"
+	"strconv"
 )
 
 func main() {
+	config := configuration.GetDefault()
 	r := gin.Default()
 
 	middleware.Register(r)
 	controller.RegisterRoutes(r)
 
-	log.Println(r.Run(":8080"))
+	log.Println(r.Run(":" + strconv.FormatInt(config.Port, 10)))
 }
