80cf5ac4872246284b6ff27a8dac005a9ddd1f79
bst27 <git@roesmann.net>
2020-08-30 13:59:53 +0200

    add step to build angular app

--- scripts/build.sh
+++ scripts/build.sh
@@ -22,10 +22,22 @@ mkdir -p "bin/linux-arm"
 mkdir -p "bin/linux-arm64"
 mkdir -p "bin/windows-amd64"
 
-# Build
+# Build Go project
 echo "==> Building..."
 env GOOS=darwin GOARCH=amd64 go build -v -o "bin/darwin-amd64" "$DIR/cmd/plaudern/"
 env GOOS=linux GOARCH=amd64 go build -v -o "bin/linux-amd64" "$DIR/cmd/plaudern/"
 env GOOS=linux GOARCH=arm go build -v -o "bin/linux-arm" "$DIR/cmd/plaudern/"
 env GOOS=linux GOARCH=arm64 go build -v -o "bin/linux-arm64" "$DIR/cmd/plaudern/"
 env GOOS=windows GOARCH=amd64 go build -v -o "bin/windows-amd64" "$DIR/cmd/plaudern/"
+
+# Build Angular project
+cd "web"
+ng build --prod --base-href "/manage/"
+
+# Copy Angular dist files
+cd "$DIR"
+cp -r "web/dist/web" "bin/darwin-amd64/"
+cp -r "web/dist/web" "bin/linux-amd64/"
+cp -r "web/dist/web" "bin/linux-arm"
+cp -r "web/dist/web" "bin/linux-arm64"
+cp -r "web/dist/web" "bin/windows-amd64"
