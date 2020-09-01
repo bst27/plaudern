2587f890c4cdc6b1d1791a229d9c29d046d18132
bst27 <git@roesmann.net>
2020-09-01 19:31:56 +0200

    add endpoint to manage comments

--- internal/controller/manage.go
+++ internal/controller/manage.go
@@ -2,8 +2,12 @@ package controller
 
 import (
 	"github.com/bst27/plaudern/internal/configuration"
+	"github.com/bst27/plaudern/internal/database"
+	"github.com/bst27/plaudern/internal/model/api/response"
+	"github.com/bst27/plaudern/internal/model/comment"
 	"github.com/gin-gonic/gin"
 	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -16,4 +20,17 @@ func registerManageRoutes(r *gin.Engine, config *configuration.Config) {
 
 	manage := r.Group("/manage")
 	manage.Static("/app", filepath.Join(filepath.Dir(execFile), "web"))
+
+	manage.GET("/comment", func(ctx *gin.Context) {
+		db := database.Get()
+
+		cmnts, err := comment.GetAll(db)
+		if err != nil {
+			log.Println(err)
+			ctx.JSON(http.StatusInternalServerError, gin.H{})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, response.NewGetComments(cmnts))
+	})
 }
