b92c8c3e6ce5a3964ef8fa9e30928b7d3c8d3734
bst27 <git@roesmann.net>
2020-09-16 17:04:55 +0200

    add auth guard

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {CommentsComponent} from './components/comments/comments.component';
 import {CommentDetailsCardComponent} from './components/comment-details-card/comment-details-card.component';
+import {AuthGuard} from './services/auth.guard';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/comments' },
-  { path: 'comments', component: CommentsComponent },
+  { path: 'comments', component: CommentsComponent, canActivate: [AuthGuard] },
   { path: 'comments/:commentId', component: CommentDetailsCardComponent },
   { path: '**', redirectTo: '/comments' }// Fallback: redirect to homepage if route does not exist
 ];
