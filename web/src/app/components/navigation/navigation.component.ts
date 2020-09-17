4434ba658c491c831853b96ce5d653d8dc40d370
bst27 <git@roesmann.net>
2020-09-17 16:22:12 +0200

    add logout to navbar

--- web/src/app/components/navigation/navigation.component.ts
+++ web/src/app/components/navigation/navigation.component.ts
@@ -1,14 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+import {AuthService} from '../../services/auth.service';
+import {MatDialog} from '@angular/material/dialog';
+import {LoadingDialogComponent} from '../loading-dialog/loading-dialog.component';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+
+  isLoggedIn: boolean;
 
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
@@ -16,6 +21,24 @@ export class NavigationComponent {
       shareReplay()
     );
 
-  constructor(private breakpointObserver: BreakpointObserver) {}
+  constructor(
+    private breakpointObserver: BreakpointObserver,
+    private authService: AuthService,
+    private loadingDialog: MatDialog,
+  ) {}
+
+  ngOnInit(): void {
+    this.authService.isLoggedIn().subscribe(loggedIn => {
+      this.isLoggedIn = loggedIn;
+    });
+  }
+
+  logout(): void {
+    const dialog = this.loadingDialog.open(LoadingDialogComponent);
+
+    this.authService.logout().subscribe(() => {
+      dialog.close();
+    });
+  }
 
 }
